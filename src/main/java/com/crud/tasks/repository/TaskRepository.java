13fd370376d6fa53183a2a6c383efa8d53089f8a
KarKiKo <karolina.kidon@gmail.com>
2018-04-15 11:32:34 +0200

    Module 15.3 HTTP method DELETE

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -4,11 +4,19 @@ import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
+
+    void deleteById(Long id);
 
     @Override
     List<Task> findAll();
 
-    Task findTaskById(Long id);
+    @Override
+    Task save(Task task);
+
+    Optional<Task> findById (Long id);
+
+
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -5,7 +5,11 @@ import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
+
+@Transactional
 @Service
 public class DbService {
     @Autowired
@@ -15,7 +19,15 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task findTask(Long id){
-        return repository.findTaskById(id);
+    public Optional<Task> getTask(final Long id){
+        return repository.findById(id);
+    }
+
+    public Task saveTask(final Task task){
+        return repository.save(task);
+    }
+
+    public void deleteTask(Long id){
+        repository.deleteById(id);
     }
 }
