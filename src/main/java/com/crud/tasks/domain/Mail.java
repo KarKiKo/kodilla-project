bc61714a470a30794cdd9a2de507844a223676d5
KarKiKo <karolina.kidon@gmail.com>
2018-06-23 18:55:35 +0200

    Last commit

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -12,4 +12,20 @@ public class Mail {
     private String subject;
     private String message;
     private String toCc;
+
+    public String getMailTo() {
+        return mailTo;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getToCc() {
+        return toCc;
+    }
 }
